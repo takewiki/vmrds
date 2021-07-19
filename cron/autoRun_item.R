f2b89f7217e160cce00c78feffbada60cb72995e
takewiki <hulilei@takewiki.com.cn>
2021-07-19 11:52:23 +0800

    add for test.

--- cron/autoRun_item.R
+++ cron/autoRun_item.R
@@ -8,9 +8,13 @@ library(cronR)
 
 # conn_erp = conn_vm_erp_prd2()
 # conn_plm = conn_vm_plm_prd2()
+# conn_erp <- vmrdspkg::conn_config_read("/srv/shiny-server/config/conn_erp.xlsx")
+# conn_plm <- vmrdspkg::conn_config_read("/srv/shiny-server/config/conn_plm.xlsx")
 
-conn_erp <- vmrdspkg::conn_config_read("/srv/shiny-server/config/conn_erp.xlsx")
-conn_plm <- vmrdspkg::conn_config_read("/srv/shiny-server/config/conn_plm.xlsx")
+conn_erp <- vmrdspkg::conn_config_read("/srv/shiny-server/config/conn_erp_rds.xlsx")
+conn_plm <- vmrdspkg::conn_config_read("/srv/shiny-server/config/conn_plm_rds.xlsx")
+
+print('rds-hello-item!')
 
 sync_PLMtoERP_Item_periodly(conn_plm = conn_plm,conn_erp = conn_erp )
 
